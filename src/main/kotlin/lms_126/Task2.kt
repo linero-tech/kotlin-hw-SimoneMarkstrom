aeb38d5281a38f9a69697caba9dc7c6fef81a7e8
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-08-30 07:30:23 +0200

    Update Task2.kt

--- src/main/kotlin/lms_126/Task2.kt
+++ src/main/kotlin/lms_126/Task2.kt
@@ -3,5 +3,6 @@ package lms_126
 fun task2()
 { */ INPUT get the area of the rectangle
     //INPUT get the height of the rectangle
+// get the base of the rectangle 
 //multiply base time height
 //OUTPUT the result to the user */ }
