1eb5f6d63640c7ba5afe6713acbd7149e2a40474
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-08-30 09:22:12 +0200

    Task 2 3

--- src/main/kotlin/lms_126/Task3.kt
+++ src/main/kotlin/lms_126/Task3.kt
@@ -1,11 +1,12 @@
 package lms_126
 
-fun task3() {
-    }
+fun task3(){
 
-/* INPUT get the base of the triangle
+
+
+//INPUT get the base of the triangle
 //INPUT get the base height of the triangle
 //Multiply base height
 //Divide result by two
-//OUTPUT the result to the user */
+//OUTPUT the result to the user */ }
 
