758cde697cecb5d57be9fc7adac21aa2043ccfcf
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-09-13 08:50:05 +0200

    Update Task6.kt

--- src/main/kotlin/lms_127/Task6.kt
+++ src/main/kotlin/lms_127/Task6.kt
@@ -2,5 +2,15 @@ package lms_127
 
 fun task6(base1: Double, base2: Double, height: Double): Double {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    // write how to calculate a trapezoid
+    val result = (base1 + base2) * height * 0.5
+    //write the result
+
+    println (result)
+
+    return result
+}
+
+fun main () {
+    task6(base1=10.0, base2=20.0, height=1.0)
 }
