9c3b06f903baf1269638e2bd24c7e224937e8df6
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-09-13 10:39:25 +0200

    Update Task7.kt

--- src/main/kotlin/lms_127/Task7.kt
+++ src/main/kotlin/lms_127/Task7.kt
@@ -2,5 +2,27 @@ package lms_127
 
 fun task7(sentence: String): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+
+    //initialize a sentence
+    var sentence = "sentence"
+
+    //count the number of the sentence
+
+    val numberOfCharacters = sentence.length
+
+    val result = numberOfCharacters
+
+
+    println("$numberOfCharacters")
+
+
+    return result
+
+
+
+}
+
+fun main(){
+
+    task7("")
 }
