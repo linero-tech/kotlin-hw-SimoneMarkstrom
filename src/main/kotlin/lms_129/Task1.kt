682773149f7d67ab6fc0f41ffcecf7549add60fb
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-09-27 10:45:20 +0200

    task 1

--- src/main/kotlin/lms_129/Task1.kt
+++ src/main/kotlin/lms_129/Task1.kt
@@ -1,6 +1,37 @@
-package lms_129
+ package lms_129
 
 fun task1(a: Int, b: Int): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+
+
+    var result = 0
+
+    if (a < b) for (number in a..b)
+
+        result += number
+    println("$result")
+
+    return result
+
+
+
 }
+
+fun main (){
+    task1(a=1, b=5)
+}
+
+
+
+
+
+
+
+
+
+/*
+ // Adding numbers from 1 to 10
+ var totalSum = 0
+ for (number in 1..10) {
+     totalSum = totalSum + number
+ }*/
