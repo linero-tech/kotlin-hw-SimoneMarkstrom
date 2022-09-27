259054910adf6bce0112a51a60b5ae0ef850da2c
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-09-27 10:46:52 +0200

    Update Task2.kt

--- src/main/kotlin/lms_129/Task2.kt
+++ src/main/kotlin/lms_129/Task2.kt
@@ -2,22 +2,6 @@ package lms_129
 
 fun task2(number: Int): Boolean {
 
-    // save it in a variable result
-    //number must be higher than 1 and number/number
-    // result number
-    //use if
-    // print result
-
-
-    val number =
-
-
-
-
-
-
-
-
 
     return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
 
