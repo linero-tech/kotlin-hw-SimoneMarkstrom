fcbd08267266b3f27a9191795e0da90aa6e55a16
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-09-28 08:11:33 +0200

    task 1, 2, 3

--- src/main/kotlin/lms_129/Task2.kt
+++ src/main/kotlin/lms_129/Task2.kt
@@ -3,6 +3,48 @@ package lms_129
 fun task2(number: Int): Boolean {
 
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = false
 
+    for (i in 2.. number/2)
+
+        if(number % 2 == 0){
+            result= true
+             break
+        }
+
+    if (!result)
+        println("$number is a primenumber")
+
+    else
+        println("$number is not a primenumber")
+
+
+ return result
+
+     }
+
+
+fun main (){
+    task2(number = 6)
 }
+
+
+
+/*
+fun main(args: Array<String>) {
+    val num = 29
+    var flag = false
+    for (i in 2..num / 2) {
+        // condition for nonprime number
+        if (num % i == 0) {
+            flag = true
+            break
+        }
+    }
+
+    if (!flag)
+        println("$num is a prime number.")
+    else
+        println("$num is not a prime number.")
+}
+ */
