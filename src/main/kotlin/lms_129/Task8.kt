198997059a80572fd27fdfe79ed9227e35c7c223
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-09-29 15:01:33 +0200

    Update Task8.kt

--- src/main/kotlin/lms_129/Task8.kt
+++ src/main/kotlin/lms_129/Task8.kt
@@ -2,6 +2,10 @@ package lms_129
 
 fun task8(number: Int): Int {
 
+    val result = 0
+    
 
     return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
 }
+
+
