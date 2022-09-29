1f3b526364f2d03a7926bf4e3cc0dd7ba7d4e492
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-09-29 14:52:15 +0200

    Update Task4.kt

--- src/main/kotlin/lms_129/Task4.kt
+++ src/main/kotlin/lms_129/Task4.kt
@@ -2,18 +2,17 @@ package lms_129
 
 fun task4(): Int {
 
- var totalSum= 0
-    for(number in 1..10000){
-
-        if (number%9==0){
-            totalSum= totalSum + number
+    var totalSum= 0
+    for (number in 1..1000) {
+        if(number%9==0){
+            totalSum= totalSum+number
         }
     }
+
     return totalSum
 
 }
-
 fun main(){
-     task4()
+    println(task4())
 
 }
