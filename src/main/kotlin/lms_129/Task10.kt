e467b3f99df4cfe987501770f59faec868919fc9
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-09-30 11:58:35 +0200

    Update Task10.kt

--- src/main/kotlin/lms_129/Task10.kt
+++ src/main/kotlin/lms_129/Task10.kt
@@ -3,5 +3,28 @@ package lms_129
 fun task10(password: String): Boolean {
 
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result= false
+
+
+    val pattern = "[a-zA-Z0-9._-]{6,10}+@".toRegex()
+
+    password.forEach {
+        password
+
+        if (pattern.matches(password)) {
+
+           result= true
+        } else {
+            result = false
+
+        }
+
+    }
+
+
+    return result
+}
+
+fun main(){
+    println(task10(password="hej0122$@"))
 }
