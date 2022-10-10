be7ae45d4c7d3ce9021bf02f901bff6f163953fb
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-10-10 12:35:47 +0200

    task 3

--- src/main/kotlin/lms_130/Task2.kt
+++ src/main/kotlin/lms_130/Task2.kt
@@ -2,5 +2,29 @@ package lms_130
 
 fun task2(items: List<Int>): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    // set result to 0
+    var result = 0
+
+    // create if its not empty
+    if(items.isNotEmpty())
+        items.forEachIndexed { index, number ->
+            if( index % 2 == 0){
+                result += number
+
+            }
+
+
+            //get the even numbers in a list
+            // add  number with result
+        }
+
+
+     println ( result )
+    return result
+}
+
+
+
+fun main(){
+    task2(listOf(1,2,3,4))
 }
--- src/main/kotlin/lms_130/Task3.kt
+++ src/main/kotlin/lms_130/Task3.kt
@@ -2,5 +2,14 @@ package lms_130
 
 fun task3(items: List<Int>): List<Int> {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    // change list to a set
+   val result = items.toSet()
+
+  println(result)
+
+    return result.toList()
+}
+
+fun main(){
+    task3(listOf(1,5,6,6,2,3,4,5))
 }
