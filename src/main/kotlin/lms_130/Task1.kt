4d5c18ea3125aeb5e47f3ebf5fffd94305bd02a0
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-10-10 09:14:14 +0200

    Update Task1.kt

--- src/main/kotlin/lms_130/Task1.kt
+++ src/main/kotlin/lms_130/Task1.kt
@@ -2,5 +2,19 @@ package lms_130
 
 fun task1(items: List<Int>): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+
+    var result = if (items.isEmpty()){
+        0
+    }else {
+        val randomIndex= (0..items.lastIndex).random()
+       items[randomIndex]
+    }
+
+
+
+    return result
+}
+
+fun main(){
+    println(task1(listOf()))
 }
