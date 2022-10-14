64bd402a5802c4df6bca0aac9cb81a6dea8b666e
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-10-14 13:17:24 +0200

    task 13

--- src/main/kotlin/lms_130/Task12.kt
+++ src/main/kotlin/lms_130/Task12.kt
@@ -2,15 +2,36 @@ package lms_130
 
 fun task12_1(sells: List<Int>): String {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val days = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    val maxValue = sells.indexOf(sells.maxOrNull())
+    val result = days[maxValue]
+
+    return result
+
+
 }
 
 fun task12_2(sells: List<Int>): String {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val days = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    val minValue = sells.indexOf(sells.minOrNull())
+    val result = days[minValue]
+
+    return result
+
 }
 
 fun task12_3(sells: List<Int>): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val result= sells.sum()
+
+    return result
+}
+
+
+fun main(){
+    println(task12_1(listOf(20,100,800,768,765,90,10)))
+    println(task12_2(listOf(20,100,800,768,765,90,10)))
+    println(task12_3(listOf(20,100,800,768,765,90,10)))
+
 }
--- src/main/kotlin/lms_130/Task13.kt
+++ src/main/kotlin/lms_130/Task13.kt
@@ -2,35 +2,60 @@ package lms_130
 
 fun task13_1(customers: List<String>): List<String> {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val result = customers.toSet().toList()
+    return result
+
 }
 
-fun task13_2(customers: List<String>): List<String> {
+    fun task13_2(customers: List<String>): List<String> {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
-}
+        val result = mutableSetOf<String>()
+        val uniqueList = mutableListOf<String>()
 
-fun task13_3(customers: List<String>): List<String> {
+        for (mail in customers) {
+            if (mail in uniqueList) {
+                result.add(mail)
+            } else {
+                uniqueList.add(mail)
+            }
+        }
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
-}
+        return result.toList()
 
-fun main() {
-    val listOfCustomer = listOf(
-        "tgundrey1l@prlog.org",
-        "bgrix1u@apache.org",
-        "mbreakspear1v@wordpress.com",
-        "cdalli1w@ft.com",
-        "rclayborn1x@mtv.com",
-        "rclayborn1x@mtv.com",
-        "jchidlow1y@nasa.gov",
-        "jchidlow1y@nasa.gov",
-        "kovell1z@washingtonpost.com",
-        "kovell1z@washingtonpost.com",
-        "kovell1z@washingtonpost.com"
-    )
+    }
 
-    println("The customers who purchased from your product: ${task13_1(listOfCustomer)}")
-    println("The customers that purchased multiple times include: ${task13_2(listOfCustomer)}")
-    println("The companies that purchased from you include: ${task13_3(listOfCustomer)}")
-}
+    fun task13_3(customers: List<String>): List<String> {
+
+
+
+        val result = mutableSetOf<String>()
+        val unique = customers.toSet()
+
+        unique.forEach{domain ->
+            val (name, last) = domain.split("@")
+            val (company, end) = last.split(".")
+            result.add(company)
+        }
+        return result.toList()
+
+    }
+
+    fun main() {
+        val listOfCustomer = listOf(
+            "tgundrey1l@prlog.org",
+            "bgrix1u@apache.org",
+            "mbreakspear1v@wordpress.com",
+            "cdalli1w@ft.com",
+            "rclayborn1x@mtv.com",
+            "rclayborn1x@mtv.com",
+            "jchidlow1y@nasa.gov",
+            "jchidlow1y@nasa.gov",
+            "kovell1z@washingtonpost.com",
+            "kovell1z@washingtonpost.com",
+            "kovell1z@washingtonpost.com"
+        )
+
+        println("The customers who purchased from your product: ${task13_1(listOfCustomer)}")
+        println("The customers that purchased multiple times include: ${task13_2(listOfCustomer)}")
+        println("The companies that purchased from you include: ${task13_3(listOfCustomer)}")
+    }
