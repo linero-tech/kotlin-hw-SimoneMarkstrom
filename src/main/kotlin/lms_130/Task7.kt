8f011a1fdce5f82de56fa09134ef497aed907fda
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-10-16 20:10:57 +0200

    Update Task7.kt

--- src/main/kotlin/lms_130/Task7.kt
+++ src/main/kotlin/lms_130/Task7.kt
@@ -5,32 +5,34 @@ fun task7(items: List<Int>): Int {
 
 
 
-        var minVal = items.first()
-        var maxVal = items.first()
-        if(items.isEmpty()){
-            var result = 0 
-        }
-
-        if(items.isNotEmpty()) {
-        items.forEach { number ->
-            if (number > maxVal) {
-                maxVal = number
-
-            }else if( number < minVal){
-                minVal = number
-                var result = minVal
-            }
-        }
 
 
-         var result = minVal
+
+                var result = 0
+
+              if(items.isNotEmpty()) {
+
+                  result = items.first()
+                  items.forEach { number ->
+
+                      if (number < result) {
+                          result = number
+
+
+                      }
+                  }
+              }
+
+
+
+return result
 
 
     }
 
-    return minVal
-}
+
+
 
 fun main(){
-   println(task7(items= listOf(1,2,3,4,5)))
+   println(task7(items= listOf()))
 }
