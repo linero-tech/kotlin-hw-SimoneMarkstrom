aaadd5e796581573bfa765073eddf95bff015ab2
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-11-01 10:13:14 +0100

    Update Task7.kt

--- src/main/kotlin/lms_131/Task7.kt
+++ src/main/kotlin/lms_131/Task7.kt
@@ -2,5 +2,16 @@ package lms_131
 
 fun task7(words: List<String>): Map<Int, String> {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = mutableMapOf<Int, String>()
+
+    words.forEachIndexed { index, word ->
+        result[index] = word
+
+    }
+
+    return result
+}
+
+fun main(){
+    println(task7(words= listOf("I", "love", "Kotlin")))
 }
