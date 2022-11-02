309c12ffa21d38cec8023fbab21c11313317e2aa
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-11-02 10:24:44 +0100

    Update Task9.kt

--- src/main/kotlin/lms_131/Task9.kt
+++ src/main/kotlin/lms_131/Task9.kt
@@ -2,5 +2,28 @@ package lms_131
 
 fun task9(grades: Map<String, List<Double>>): Double {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = 0.0
+    val newGrades = grades.values
+    val totalSum = mutableListOf<Double>()
+    var gradeSum = 0.0
+
+    newGrades.forEach{numbers ->
+        var total = 0.0
+        numbers.forEach{number ->
+            total += number
+            gradeSum++
+        }
+        totalSum += total
+    }
+    totalSum.forEach{sum ->
+        result += sum/gradeSum
+    }
+
+    return result
 }
+
+fun main(){
+    println(task9(grades = mapOf("Ana" to listOf(4.0, 4.5, 5.0), "John" to listOf(5.0, 5.0, 3.0), "Lise" to listOf(5.0, 5.0, 5.0))))
+}
+
+   
