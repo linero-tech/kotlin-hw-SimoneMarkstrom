9110796feb1eb3a20bc20625019b56dc3a383e5c
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-11-04 13:07:16 +0100

    Update Task1.kt

--- src/main/kotlin/lms_131/Task1.kt
+++ src/main/kotlin/lms_131/Task1.kt
@@ -4,7 +4,7 @@ fun task1(): Map<Int, Int> {
 
     val result = mutableMapOf<Int,Int>()
 
-    for (number in 1..20) {
+    for (number in 10..20) {
         if (number % 2 != 0) {
             result[number] = number * 2
         }
