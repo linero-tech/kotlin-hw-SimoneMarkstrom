1bc84ddedd279945da466b4cf99dd3000eccf906
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-11-04 12:59:15 +0100

    task 15

--- src/main/kotlin/lms_131/Task12.kt
+++ src/main/kotlin/lms_131/Task12.kt
@@ -3,7 +3,7 @@ package lms_131
 fun task12(a: List<Int>, b: List<String>): Map<Int, String> {
 
     val result = mutableMapOf<Int, String>()
-    
+
     a.zip(b).forEach { pair ->
         result += pair
     }
