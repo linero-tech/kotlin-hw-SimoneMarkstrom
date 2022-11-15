a6a6cd8e14d4a004218072824a5760ae328633f4
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-11-15 08:27:51 +0100

    Update Task3.kt

--- src/main/kotlin/lms_132/Task3.kt
+++ src/main/kotlin/lms_132/Task3.kt
@@ -8,13 +8,7 @@ fun task3(sentence: String?): String {
         result += "*"
     }else{ result= sentence.replace(Regex("['a''e''i''o''u''A''E''I'O'U']"),"*")
     }
-    //if the letter is null assign it to be *
-
-    // else the letter is  a, e, i , o , u  change it to be * (make it case sensitive)
-
-
-
-
+    
     return result.toString()
 }
 
