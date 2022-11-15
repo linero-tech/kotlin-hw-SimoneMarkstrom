d84a9f58dcc54b23732196830d55ac65a80e0947
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-11-15 08:22:02 +0100

    Update Task3.kt

--- src/main/kotlin/lms_132/Task3.kt
+++ src/main/kotlin/lms_132/Task3.kt
@@ -2,5 +2,32 @@ package lms_132
 
 fun task3(sentence: String?): String {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+     var result = ""
+    var vowels = listOf('a', 'e', 'i' ,'o','u')
+    if(sentence == null){
+        result += "*"
+    }else{ result= sentence.replace(Regex("['a''e''i''o''u''A''E''I'O'U']"),"*")
+    }
+    //if the letter is null assign it to be *
+
+    // else the letter is  a, e, i , o , u  change it to be * (make it case sensitive)
+
+
+
+
+    return result.toString()
 }
+
+fun main() {
+    println(task3(sentence = null))
+}
+
+
+
+/*
+fun main() {
+    val input = "H...H"
+    val output = input.replace('.', 'B')
+    println(output)  // Prints "HBBBH"
+}
+*/
