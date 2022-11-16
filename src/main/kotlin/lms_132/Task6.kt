ffaa8cbd75e4a2bbabd3bbae579bb8d5228b0a60
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-11-16 12:28:05 +0100

    Update Task6.kt

--- src/main/kotlin/lms_132/Task6.kt
+++ src/main/kotlin/lms_132/Task6.kt
@@ -2,23 +2,21 @@ package lms_132
 
 fun task6(items: List<Int?>): List<Int?> {
 
-    val result = mutableListOf<Int?>()
+        val result = mutableListOf<Int?>()
 
-    if(items.isEmpty()){
-        result += result
-    }else {
-        items.forEachIndexed { index, items ->
-            if (items != null) {
-                result.add(items * index)
-            }else{
-                result.add(items)
+        if(items.isEmpty()){
+            result += result
+        }else {
+            items.forEachIndexed { index, items ->
+                if (items != null) {
+                    result.add(items * index)
+                }else{
+                    result.add(items)
+                }
             }
         }
+        return result
     }
-    return result
-}
-fun main(){
-    println(task6(items = listOf(1, 2, null, 4)))
-}
-
-    
+    fun main() {
+        println(task6(items = listOf(1, 2, null, 4)))
+    }
