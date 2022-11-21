851f6221406cd7b4042a5125b3b108b6ab8fbca6
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-11-21 10:24:10 +0100

    Update Task4.kt

--- src/main/kotlin/lms_132/Task4.kt
+++ src/main/kotlin/lms_132/Task4.kt
@@ -3,21 +3,16 @@ package lms_132
 fun task4(sentence: String?): Map<Char, Int> {
 
     val result = mutableMapOf('a' to 0, 'e' to 0, 'i' to 0, 'o' to 0, 'u' to 0)
-    val newSentence = sentence?.toLowerCase()
-    //If sentence is null = empty map
-    if (newSentence.isNullOrEmpty()) {
+
+    if (sentence.isNullOrEmpty()) {
         result.clear()
     }else{
-        newSentence.forEach{letter ->
+        sentence?.lowercase().forEach{letter ->
             if (letter in result.keys) {
                 result[letter] = result.getOrDefault(letter, 0)+1
             }
         }
     }
-
-
-    // Else count vowels in sentence
-    // add vowels as key to a map and number of times they appear to value
 
 
     return result
