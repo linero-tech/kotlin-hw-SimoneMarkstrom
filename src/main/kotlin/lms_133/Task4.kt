52a05b0dd6a7bf33e7dd7b5952c2cbf79f94daa4
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-11-28 09:38:20 +0100

    task 4

--- src/main/kotlin/lms_133/Task4.kt
+++ src/main/kotlin/lms_133/Task4.kt
@@ -1,3 +1,16 @@
 package lms_133
 
-// TODO(Create your function here)
+fun washing(times: Int, months: Int): Double {
+    val timesPerMonth = times * 30
+    val timesAllMonths = timesPerMonth * months
+    val seconds = timesAllMonths * 21
+
+    return seconds / 60.0
+
+}
+
+
+//830= 240, gånger per dag 1 månad
+//240 * 7 = 1680 // svar från ovan * antal månader
+//1680 * 21 = 35280// antal gånger tvätt * 21 sekunder
+//35280/60 = 588// sekunder / 60 = antal minuter
