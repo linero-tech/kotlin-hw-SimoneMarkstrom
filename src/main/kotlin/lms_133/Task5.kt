0423b3704747eac68f20220dd54fdb9c0020bf30
SimoneMarkstrom <111756656+SimoneMarkstrom@users.noreply.github.com>
2022-12-02 07:31:26 +0100

    Update Task5.kt

--- src/main/kotlin/lms_133/Task5.kt
+++ src/main/kotlin/lms_133/Task5.kt
@@ -1,15 +1,17 @@
 package lms_133
 
-
 fun groceries(groceryList: List<Map<String, Any>>): Double{
     var result = 0.0
+    var totalQuantity = 0
+    var totalPrice  = 0.0
     if(groceryList.isEmpty()){
         result
     }else {
         groceryList.forEach { input ->
-            val input1 = input.getValue("quantity")
-            val input2 = input.getValue("price")
-            result = input1 as Int * input2 as Double
+            totalQuantity = input.getValue("quantity") as Int
+            totalPrice = input.getValue("price") as Double
+            val total = totalQuantity * totalPrice
+            result += total
         }
     }
     return result
@@ -17,7 +19,8 @@ fun groceries(groceryList: List<Map<String, Any>>): Double{
 
 fun main(){
     println(groceries(groceryList =
-    listOf(mapOf("product" to "milk", "quantity" to 1, "price" to 1.50))))
+    listOf(mapOf("product" to "milk", "quantity" to 3, "price" to 1.50),
+        mapOf("product" to "meat", "quantity" to 3, "price" to 2.50))))
 }
 
 
@@ -25,7 +28,3 @@ fun main(){
 // If list is empty return 0.0
 // Else count how many items you should buy and multiply with cost
 // Return total cost of all groceries
-
-
-
-// TODO(Create your function here)
